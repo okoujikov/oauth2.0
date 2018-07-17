a7a2bd550b5ac4617a5ae4dd55974e8ead24e2e1
okoujikov <40499539+okoujikov@users.noreply.github.com>
2018-07-17 12:59:15 -0700

    Create services.py

---
+++ services.py
@@ -0,0 +1,8 @@
+from requests_oauth2 import OAuth2
+
+class DrChronoClient(OAuth2):
+    # TODO: search API documentation to find this stuff
+    site = " "
+    authorization_url = " "
+    token_url = " "
+    scope_sep = " "
