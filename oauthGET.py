66addd330c456f8173f33e885d4d7befee91607a
okoujikov <40499539+okoujikov@users.noreply.github.com>
2018-07-17 12:58:37 -0700

    Update oauthGET.py

--- oauthGET.py
+++ oauthGET.py
@@ -1,41 +1,73 @@
 import requests
+import os
+import jwt
+import datetime
+# QUESTION: would it be best to design this program for python or python3? Assuming python would be more accessible for users --> Answer: python3 might be best
 
-# URL = "https://drchrono.com/"
+from services import DrChronoClient
+from requests_oauth2 import OAuth2BearerToken
+from flask import Flask, jsonify, make_response, request, redirect, session
+from functools import wraps # used for protected and unprotected routes
+
+app = Flask(__name__) # Placeholder for the oauthGET.py module. QUESTION: generally what is a placeholder??
+app.secret_key = os.environ.get("secret_key") or os.urandom(20) # this is the secret key for your flask webserver
+# this line accesses an environment variable using os.environ or it generates a secret_key randomly
+# Q: where does secret_key come from? --> A: we are generating it with os.urandom(20)
+# For more info: http://flask.pocoo.org/docs/1.0/quickstart/#sessions
 
 
-app = Foo(__name__) # Foo is the client's application
-app.secret_key = os.environ.get("secret_key") or os.urandom(20) # this line accesses an environment variable using os.environ or it generates a secret_key randomly
-# QUESTION: where does secret_key come from?
-
-
-drchrono_auth = DrChronoClient( #TODO: define DrChronoClient in a seperate file
-    client_id = ("sny2QrTbzRdPR5KiAq252KgcWU0fO22QRdh7wNUJ"), # QUESTION: do I need parentheses here?
-    client_secret = "I9baXj5l4I4dzCUgXMqgZsOUrxq2wm2GurggEGbzNKweiRIe5LwcefWvK7TlUHhSDgOHYtGRi1tmvczN3uCjf2TOb2YW7202Fhbf1A5iG6azQiWnDbFY3Xb8VghUpMvj",
+drchrono_auth = DrChronoClient( # passing in client_id, client_secret, redirect_uri
+    client_id = os.getenv('CLIENT_ID'),
+    client_secret = os.getenv('CLIENT_SECRET'),
     redirect_uri = "http://localhost:8080/complete/drchrono/",
 )
 
+app.config['SECRET_KEY'] = 'thisisthesecretkey' # this is the secret key for now
 
-# For now DrChronoClient() exists here
-# TODO: complete DrChronoClient(). Outline taken from services.py
-class DrChronoClient(OAuth2):
-    site = "https://accounts.google.com"
-    authorization_url = "/o/oauth2/auth"
-    token_url = "/o/oauth2/token"
-    scope_sep = " "
-#-------------------------------------
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') #http://127.0.0.1:5000/route?token=asefjkldsasdfkljaskdfj
+
+        if not token:
+            return jsonify({'message' : "Token is missing!"}), 403
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message' : "Token is invalid!"}), 403
+
+        return f(*args, **kwargs)
+    return decorated
+
+# QUESTION: Do I need unprotected and protected routes? Or will everything be protected?
+@app.route('/unprotected')
+def unprotected():
+    return jsonify({'message' : "Anyone can view this!"})
+
+@app.route('/protected')
+@token_required
+def protected():
+    return jsonify({'message' : "This is only available for people with a valid token."})
 
 @app.route("/")
 def index():
-    print("calling index()") # this line is used to text index() TODO: delete me
-    return redirect("/drchrono/")
-    # QUESTION: wtf is happening here? --> we are define index() to return something...
-    # looks like a function called redirect which holds the string "/drchrono/"
+    return redirect("/login") #this was /drchrono/
 
+@app.route("/login") # get login information for authorization
+def login():
+    auth = request.authorization
+
+    if auth and auth.password =="password":
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY']) # creates a token for the user
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response("Could not verify!", 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
 
 @app.route("/drchrono/")
 def drchrono_index():
     if not session.get("access_token"):
-        return redirect("/drchrono/oauth2callback")
+        return redirect("/drchrono/oauth2callback") # TODO: redirect to DrChrono login
     with requests.Session() as s: # QUESTION: what does "as" do?
         s.auth = OAuth2BearerToken(session["access_token"])
         print("running drchrono_index()")
@@ -45,3 +77,6 @@ def drchrono_index():
     return "Hello, {}!".format(data["displayName"])
 
 # TODO: oauth2callback
+
+if __name__ == "__main__":
+    app.run(debug=True)
