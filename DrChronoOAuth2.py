bd48207deae31a9117aa3a8145ef0bb9e0a0ab26
okoujikov <40499539+okoujikov@users.noreply.github.com>
2018-08-07 14:43:33 -0700

    Update DrChronoOAuth2.py
    Added a /drchrono/login route and implemented it in /drchrono/oauth2callback

--- DrChronoOAuth2.py
+++ DrChronoOAuth2.py
@@ -5,42 +5,53 @@ from apiEndpoints import AppointmentEndpoint
 from flask import Flask, request, redirect, session
 
 drchrono_auth = DrChronoClient( # drchrono_auth is used to pass in client_id, client_secret, redirect_uri.
-    #  These values can be entered manually below or through the terminal for privacy/security. How it's currently set up, these values need to be passed in with export statements.
+    # These values can be entered manually below or through the terminal for privacy/security.
+    # How it's currently set up, these values need to be passed in with export statements.
+    # client_id and client_secret are given to the developer by drchrono through the drchrono website
     client_id = os.getenv('CLIENT_ID'),
     client_secret = os.getenv('CLIENT_SECRET'),
+    # redirect_uri is chosen by the developer and configured in the drchrono website
     redirect_uri = "http://localhost:5000/drchrono/oauth2callback",
 )
 
 app = Flask(__name__) # initialize the Flask framework
 
-app.config['SECRET_KEY'] = 'thisisthesecretkey'
+app.config['SECRET_KEY'] = os.urandom(32) # set the secret key
 
-@app.route("/")
+@app.route("/") # redirects the user to /drchrono/
 def index():
-    return redirect("/drchrono/") # redirects user to /drchrono/
+    return redirect("/drchrono/")
 
-@app.route("/drchrono/")
+@app.route("/drchrono/") # checks for an access token and uses it to return data from the appointments endpoint
 def drchrono_index():
-    if not session.get("access_token"): # if there is no access token then client is redirected to /drchrono/oauth2callback
-        return redirect("/drchrono/oauth2callback")
-    api = AppointmentEndpoint(session.get('access_token')) # if there is an access token, we use it to access the Appointment Endpoint
+    if not session.get("access_token"): # if there is no access token then client is redirected to /drchrono/login
+        return redirect("/drchrono/login")
+    # if the access token is available, we use it to access the Appointment Endpoint
+    api = AppointmentEndpoint(session.get('access_token'))
     data = api.list(date="2018-07-20")
-    return str(list(data)) # returns appointment data for 2018-07-20
+    return str(list(data)) # returns appointment data from the given date
 
-@app.route("/drchrono/oauth2callback")
+@app.route("/drchrono/login") # start the login process
+def drchrono_login():
+    return redirect(drchrono_auth.authorize_url(
+        scope=["calendar:read", "clinical:read"],  # these parameters are required to view appointments
+        response_type="code",
+    ))
+
+@app.route("/drchrono/oauth2callback") # finish the login process
 def drchrono_oauth2callback():
-    code = request.args.get("code") # requests a code from DrChrono TODO: is this generated in the DrChrono server?
-    error = request.args.get("error") # check for an error
+    # check for and print a friendly error message
+    error = request.args.get("error")
     if error:
-        return "error :( {!r}".format(error) # prints error if there is one TODO: can I get an error from this statement?
-    if not code:
-        return redirect(drchrono_auth.authorize_url( # if the variable code is empty, redirect and collect an access token TODO: verify
-            scope=["calendar:read", "clinical:read"], # these parameters are required to view appointments
-            response_type="code",
-        ))
-    data = drchrono_auth.get_token( # TODO: figure out what code and grant_type are ?
+        return "error :( {!r}".format(error) # prints error if there is one TODO: test if I get an error statement. Test scope, response type
+    # request to get a code from the DrChrono generated url for temporary use
+    code = request.args.get("code")
+    if not code: # if there is no code go back to the login process
+        return redirect("/drchrono/login")
+    # use the temporary code to get a semi-perminent access token
+    data = drchrono_auth.get_token(
         code=code,
-        grant_type="authorization_code",
+        grant_type="authorization_code", # describes code type - https://oauth.net/2/grant-types/authorization-code/
     )
     session["access_token"] = data.get("access_token") # sets session["access_token"] equal to a token
     return redirect("/")
@@ -53,7 +64,7 @@ def drchrono_showtoken():
 @app.route("/drchrono/logout") # route to logout of drchrono
 def drchrono_logout():
     del session["access_token"]
-    return "Congratulations logging out"
+    return "Congratulations your token has been removed"
 
 if __name__ == "__main__":
     app.run(debug=True)
