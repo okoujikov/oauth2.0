253bd3fc59633c047da2ac841933faba2e3fcb43
okoujikov <40499539+okoujikov@users.noreply.github.com>
2018-08-22 16:28:45 -0700

    Update services.py
    Updated with DrChrono services

--- services.py
+++ services.py
@@ -1,8 +1,9 @@
 from requests_oauth2 import OAuth2
 
 class DrChronoClient(OAuth2):
-    # TODO: search API documentation to find this stuff
-    site = " "
-    authorization_url = " "
-    token_url = " "
+    # TODO: search API documentation to find this stuff. DONE
+    # TODO: confirm that this is the correct documentation and it works
+    site = "https://drchrono.com"
+    authorization_url = "/o/authorize"
+    token_url = "/o/token/"
     scope_sep = " "
